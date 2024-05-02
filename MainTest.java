e80b3e8c2c356eb78f3adae232065b02fac5e968
NMWRW <NMWRW033@gmail.com>
2024-05-02 13:46:49 -0400

    Update

--- MainTest.java
+++ MainTest.java
@@ -8,14 +8,22 @@ public class MainTest extends Frame implements ActionListener{
         JFrame f = new JFrame("TEST");
 
         JButton b1 = new JButton("Members");
+        b1.setBounds(0,0, 150,20);
+        b1.setActionCommand("member");
         b1.addActionListener(this);
 
         JButton b2 = new JButton("Development Plans ");
+        b2.setBounds(150,0, 150,20);
+        b2.setActionCommand("Development Plans");
+        b2.addActionListener(this);
 
         JButton b3 = new JButton("Plats");
-
+        b3.setBounds(300,0, 150,20);
+        b3.setActionCommand("Plats");
+        b3.addActionListener(this); 
+        
         text = new JTextField();
-        text.setBounds(50,100, 150,20);
+        text.setBounds(50,0, 150,20);
 
         label=new JLabel();  
         label.setBounds(100,300, 250,20);    
@@ -35,10 +43,10 @@ public class MainTest extends Frame implements ActionListener{
 
     public void actionPerformed(ActionEvent e) {  
         try{  
-        String page = " ";
-        label.setText("NewText");
+        label.setText("The " + e.getActionCommand() + " button was pressed");
         }catch(Exception ex){System.out.println(ex);}  
-    }  
+    } 
+    
 
     public static void main(String[] args) {
         new MainTest();
